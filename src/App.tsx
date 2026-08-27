db15a9a4dd22d4c0ebadd65e505e6e7897802262
zhiyuan <czy20140714@163.com>
2026-08-27 23:08:02 +0800

    hide button

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,1 @@
-import {NavLink,Outlet} from 'react-router-dom'; export function App(){return <><header><NavLink to="/">Seal Generator</NavLink><nav><NavLink to="/generator">生成器</NavLink><NavLink to="/about">关于</NavLink></nav></header><Outlet/></>}
+import {NavLink,Outlet,useLocation} from 'react-router-dom'; export function App(){const {pathname}=useLocation(); return <><header><NavLink to="/">Seal Generator</NavLink><nav>{pathname !== '/' && pathname !== '/about' && <NavLink to="/generator">生成器</NavLink>}<NavLink to="/about">关于</NavLink></nav></header><Outlet/></>}
